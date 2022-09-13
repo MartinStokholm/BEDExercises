eed55b621b231cf12ab89387ebd3a35f4eab868e
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-09-13 18:22:08 +0200

    ok

---
+++ Lesson1/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lesson1.Models;
+
+namespace Lesson1.Pages_Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Lesson1Context _context;
+
+        public DetailsModel(Lesson1Context context)
+        {
+            _context = context;
+        }
+
+      public Movie Movie { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Lesson1/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lesson1.Models;
+
+namespace Lesson1.Pages_Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Lesson1Context _context;
+
+        public IndexModel(Lesson1Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
