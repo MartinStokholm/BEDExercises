55ba5095ee5810af9fa712c1a03850d11e05eb54
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-16 16:31:48 +0100

    Spacing

--- assigment3Identity/BreakfastBuffet/BreakfastBuffetApp/wwwroot/js/KitchenUpdate.js
+++ assigment3Identity/BreakfastBuffet/BreakfastBuffetApp/wwwroot/js/KitchenUpdate.js
@@ -3,12 +3,14 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/KitchenReportService").build();
 
 connection.on("kitchenUpdate", function () {
-
     window.location.reload("kitchen");
+    
 });
 
 connection.start().then(function () {
     console.log("Connected");
+    
 }).catch(function (err) {
     console.error(err.toString());
+
 });
